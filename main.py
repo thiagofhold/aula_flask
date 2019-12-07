31d134962f951d801e46e27acf8cede24212e83d
Thiago Felipe Moreira <thiagofhold@gmail.com>
2019-12-07 11:19:30 -0300

    primeiro commit

---
+++ main.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route('/contato')
+def contato():
+	return render_template('contato.html')
+
+
+@app.route('/user/<nome>')
+def user(nome):
+	return '<h1>Hello, {}!</h1>'.format(nome)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
